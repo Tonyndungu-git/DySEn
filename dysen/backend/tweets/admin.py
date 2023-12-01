ce2674f520990b2ecac3a5374d0a0e758ef16692
Agnes Mubia <agnes.mubia@kalro.org>
2023-12-01 16:13:54 +0300

    currently using windows added likes model and applied it to admin

--- dysen/backend/tweets/admin.py
+++ dysen/backend/tweets/admin.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
-
+from .models import Tweet, TweetLike
 # Register your models here.
-from .models import Tweet
+class TweetLikeAdmin(admin.TabularInline):
+    model = TweetLike
+
 
 class TweetAdmin(admin.ModelAdmin):
+    inlines = [TweetLikeAdmin]
     list_display = ['__str__', 'user']
     search_fields = ['content', 'user__username', 'user__email']
 
--- dysen/backend/tweets/models.py
+++ dysen/backend/tweets/models.py
@@ -5,13 +5,18 @@ from django.db.models import Q
 
 User = settings.AUTH_USER_MODEL
 
+class TweetLike(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    tweet = models.ForeignKey("Tweet", on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
 class Tweet(models.Model):
     # Maps to SQL data
-    # id = models.AutoField(primary_key=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField(blank=True, null=True)
+    likes = models.ManyToManyField(User, related_name='tweet_user', blank=True, through='TweetLike')
     image = models.FileField(upload_to='images/', blank=True, null=True)
-
+    timestamp = models.DateTimeField(auto_now_add=True)
     # def __str__(self):
     #     return self.content
 
